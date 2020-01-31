8023f7be81891c52ab58de275287a4496dee6c4f
Sonja E. Hennessy <35819406+SHenn39910@users.noreply.github.com>
2020-01-30 23:42:05 -0500

    Add files via upload
    article files

---
+++ comment.js
@@ -0,0 +1,17 @@
+var mongoose = require("mongoose");
+
+
+var Schema = mongoose.Schema;
+
+var Commentchema = new Schema ({
+    title: {
+        type: String,
+    },
+    body: {
+        type: String,
+        required: true
+    },
+
+});
+var Comment = mongoose.model("Comment", CommentSchema);
+module.exports = Comment;
